076a4c5253737cd8b9c7ce99ff0ff1683e01bce6
Isaiah <isaiahgarcia227@gmail.com>
2024-10-17 11:16:11 -0500

    adjusted count minimum

--- accountability.js
+++ accountability.js
@@ -22,54 +22,46 @@ async function countRowsAndEmail() {
     await page.goto(loginUrl, { waitUntil: 'networkidle2' });
     await page.click('form button[type="submit"]');
     await page.waitForNavigation({ waitUntil: 'networkidle2' });
-    // console.log('going to login page.');
 
-    await page.type('#login_field', process.env.USERNAME);  // Adjust the selector
-    await page.type('#password', process.env.PASSWORD);  // Adjust the selector
-    // console.log('Credentials entered.');
+    await page.type('#login_field', process.env.USERNAME);  
+    await page.type('#password', process.env.PASSWORD);  
 
     await page.click('input[type="submit"]');
     await page.waitForNavigation({ waitUntil: 'networkidle2' });
 
-    // console.log('Login successful.');
-
     // Go to progress page
     await page.goto(targetUrl, { waitUntil: 'networkidle2' });
 
-    // Step 5: Count finished problems
+    // Count finished problems
     const numberOfRows = await page.evaluate(() => {
       return document.querySelectorAll('div[role="row"]').length;
     });
 
     const totalCompleted = numberOfRows - 1;
 
-    // Define filePath outside of IIFE so it's accessible
+    // Update daily progress in dailyReport.txt
     const filePath = path.join(__dirname, 'dailyReport.txt');
     let dailyCount = 0;
 
-    // Step 6: update daily progress
+    
     try {
       // Check if the file exists
       await fs.access(filePath);  
-      // console.log('Daily report file exists.');
 
-      // Read previous count and subtract today's count from previous count
+      // Record previous count (oldCount) and subtract today's count from oldCount
       const fileContent = await fs.readFile(filePath, 'utf-8');
-      // console.log('File content:', fileContent);
       const oldCount = parseInt(fileContent, 10);
       dailyCount = totalCompleted - oldCount;
       await fs.writeFile(filePath, totalCompleted.toString(), 'utf-8');
       
     } catch (error) {
-      // console.log('Creating daily report...');
       dailyCount = totalCompleted;  // If no file exists, use totalCompleted
       await fs.writeFile(filePath, totalCompleted.toString(), 'utf-8');
-      // console.log('Created dailyReport.txt with default content.');
     }
 
-    // Step 7: Determine the email subject based on daily count
+    // Determine the email subject based on daily count
     let subject;
-    if (dailyCount > 5) {
+    if (dailyCount >= 5) {
       subject = "SUCCESS: LeetCode Accountability";
     } else {
       subject = "HELP: LeetCode Accountability";
@@ -80,7 +72,7 @@ async function countRowsAndEmail() {
       `Total problems completed: ${totalCompleted}`
     ].join('\n'); 
 
-    // Step 8: Send email to accountability partner
+    // Send email to accountability partner
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -97,11 +89,9 @@ async function countRowsAndEmail() {
       text: message,  
     };
 
-    // Send the email
+    // Send email
     await transporter.sendMail(mailOptions);
-    // console.log('Email sent successfully.');
 
-    // Close the browser
     await browser.close();
   } catch (error) {
     console.error('An error occurred:', error.message);
